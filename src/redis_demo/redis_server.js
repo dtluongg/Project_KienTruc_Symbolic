4d46e71c9789f33d2d8934b9b40e69299e74df7d
dtluongg <fastdominicracing@gmail.com>
2025-05-23 16:13:05 +0700

    Done rate limit server

--- src/redis_demo/redis_server.js
+++ src/redis_demo/redis_server.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import redisProductRoutes from './api/redisProductRoutes.js';
 import mongoose from 'mongoose';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 app.use(express.json());
+
+// Giới hạn 5 request mỗi phút cho mỗi IP vào /redis/products
+const apiLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 phút
+  max: 5, // tối đa 5 request
+  message: { error: 'Bạn đã gửi quá nhiều request, vui lòng thử lại sau 1 phút.' }
+});
+
+app.use('/redis/products', apiLimiter);
 
 // Route cho CRUD Redis Product
 app.use('/redis', redisProductRoutes);
